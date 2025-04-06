f58efcf59109daeadb0d0f17b5e34e09bf659b79
Yatik1 <yatiksrivastava1@gmail.com>
2025-04-06 18:11:20 +0530

    feat: add FlexCol component for flexible column layout with drag-and-drop support

---
+++ src/components/builder/layouts/FlexCol.tsx
@@ -0,0 +1,52 @@
+
+import { useState } from "react";
+import { ElementType, getDefaultProps } from "../Canvas"
+import ComponentRenderer from "../ComponentRenderer"
+
+function FlexCol({props}:{props:any}) {
+
+  const [children, setChildren] = useState(props.children || [])
+
+  function handleDrop(e:React.DragEvent) {
+    e.stopPropagation();
+    e.preventDefault();
+    const childComponentType = e.dataTransfer.getData('componentId')
+
+    const newChildElement:ElementType = {
+      id:`${childComponentType}-${Date.now()}`,
+      type:childComponentType,
+      props:getDefaultProps(childComponentType)
+    }
+
+    setChildren((prevChildren:any) => [...prevChildren, newChildElement])
+  }
+
+  function handleDragOver(e:React.DragEvent) {
+    e.preventDefault();
+  }
+
+  return (
+    <div 
+      className={`flex flex-col gap-4 border border-gray-400 border-dashed w-full min-h-30 p-2 items-${props.alignItems} justify-${props.justifyContent} hover:border-blue-200`}
+      onDrop={handleDrop}
+      onDragOver={handleDragOver}  
+    >
+        {children.length !== 0 ? ( 
+          children.map((child:any, index:any) => (
+            <div key={index}>
+              <ComponentRenderer element={child}  />
+            </div>
+          ))
+        ) : 
+        (
+          <div className={`h-full flex items-center justify-center`}>
+            Drop component here
+          </div>
+        )
+      }
+
+    </div>
+  )
+}
+
+export default FlexCol
