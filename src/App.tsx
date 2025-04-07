aded52a2b7c55250f337c72b9942192842b88d1d
Yatik1 <yatiksrivastava1@gmail.com>
2025-04-07 17:39:27 +0530

    feat: integrate useBuilder hook to manage elements and add button for JSON data retrieval

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,12 @@
 // import { Layers } from 'lucide-react';
 import ComponentLibrary from './components/builder/ComponentLibrary';
 import Canvas from './components/builder/Canvas';
+import useBuilder from './hooks/useBuilder';
 
 function App() {
+
+  const {elements} = useBuilder() as any 
+
   return (
     <div className="min-h-screen h-auto bg-gray-50">
       
@@ -11,6 +15,7 @@ function App() {
         <ComponentLibrary />
         <Canvas />
       </main>
+      <button onClick={() => console.log(elements)}>Get data json</button>
     </div>
   );
 }
