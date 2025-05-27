999d45088dfe1acc31c940618301d4606c38ea98
Yatik1 <yatiksrivastava1@gmail.com>
2025-05-27 14:32:40 +0530

    refactor: enhance ComponentRenderer by improving layout styles and adding hover effects for better user interaction

--- src/components/builder/ComponentRenderer.tsx
+++ src/components/builder/ComponentRenderer.tsx
@@ -18,21 +18,21 @@ function ComponentRenderer({ element }: ComponentRendererProps) {
     case 'heading':
       const HeadingTag = props.level as keyof JSX.IntrinsicElements;
       if(props.text) return (
-        <div className={`w-full h-fit p-2`} style={{background:props.bgColor}}>
+        <div className={`w-fit h-fit p-2 hover:border hover:border-orange-400`} style={{background:props.bgColor}}>
           <HeadingTag className={`font-bold`} style={{color:props.color}}>{props.text}</HeadingTag>
         </div>
       )
-      else return <HeadingTag className={`text-gray-500`}>Add Heading</HeadingTag>;
+      else return <HeadingTag className={`text-gray-500 hover:border hover:border-orange-400`}>Add Heading</HeadingTag>;
 
     case 'text':
       if(props.content) {
         return(
-          <div className='w-full h-fit p-2' style={{background:props.bgColor}}>
+          <div className='w-fit h-fit p-2 hover:border hover:border-blue-400' style={{background:props.bgColor}}>
             <p style={{color:props.color, fontStyle:props.fontStyle, textDecoration:props.decoration}}>{props.content}</p>
           </div>
         )
       }
-      return <p className='text-gray-500'>Add content</p>;
+      return <p className='text-gray-500 hover:border hover:border-orange-400'>Add content</p>;
 
     case 'image':
       return (
@@ -40,11 +40,11 @@ function ComponentRenderer({ element }: ComponentRendererProps) {
           <img
             src={props.src}
             alt="uploaded image"
-            className="rounded-lg object-fit aspect-auto"
+            className="rounded-lg object-fit aspect-auto hover:border hover:border-pink-600"
             style={{width:props.width+"px", height:props.height+"px"}}
           />
         ) : (
-          <button className='w-fit p-2 rounded-md bg-gray-200 text-gray-400'>
+          <button className='w-fit p-2 rounded-md bg-gray-200 text-gray-400 hover:border hover:border-pink-600'>
             <p>Add Image</p>
           </button>
         )
@@ -52,7 +52,7 @@ function ComponentRenderer({ element }: ComponentRendererProps) {
 
     case 'button':
       return (
-        <button className={`px-4 py-2`} style={{background:props.bgColor?props.bgColor:"black", color:props.color?props.color : "white", borderRadius:props.rounded+"px"}}> 
+         <button className="px-4 py-2 hover:border hover:border-green-400" style={{background:props.bgColor?props.bgColor:"white", color:props.color?props.color : "black", borderRadius:props.rounded+"px"}}> 
           {props.text ? props.text : "Button"}
         </button>
       );
@@ -62,24 +62,24 @@ function ComponentRenderer({ element }: ComponentRendererProps) {
         <Section props={props} id={id} />
       );
 
-    case 'navbar':
-      return (
-        <nav className="flex gap-4 p-4 bg-gray-100 rounded-lg">
-          {props.links.map((link: { text: string; href: string }, index: number) => (
-            <a
-              key={index}
-              href={link.href}
-              className="text-blue-500 hover:text-blue-600"
-            >
-              {link.text}
-            </a>
-          ))}
-        </nav>
-      );
+    // case 'navbar':
+    //   return (
+    //     <nav className="flex gap-4 p-4 bg-gray-100 rounded-lg">
+    //       {props.links.map((link: { text: string; href: string }, index: number) => (
+    //         <a
+    //           key={index}
+    //           href={link.href}
+    //           className="text-blue-500 hover:text-blue-600"
+    //         >
+    //           {link.text}
+    //         </a>
+    //       ))}
+    //     </nav>
+    //   );
 
     case 'divider':
       return(
-        <div className='w-full h-full p-3'>
+        <div className='w-full p-3'>
           <hr className='text-gray-200 w-full' />
         </div>
       );
