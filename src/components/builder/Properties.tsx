09a43fb233ff418d4db4ac9d241ec1741e0aeee1
Yatik1 <yatiksrivastava1@gmail.com>
2025-05-27 14:32:47 +0530

    refactor: streamline Properties component by removing unused imports and buttons, and enhancing button layout for improved user experience

--- src/components/builder/Properties.tsx
+++ src/components/builder/Properties.tsx
@@ -1,7 +1,8 @@
-import { Delete, Trash, X } from "lucide-react"
+import { Trash, X } from "lucide-react"
 import useBuilder from "../../hooks/useBuilder"
 
 import PropertyRenderer from "./PropertyRenderer"
+import { ElementType } from "react";
 
 export default function Properties() {
 
@@ -9,7 +10,7 @@ export default function Properties() {
 
   function deleteElementById(elements:any, idToDelete:any) {
     return elements
-      .map((element:any) => {
+      .map((element:ElementType | any) => {
         if (element.props?.children) {
           return {
             ...element,
@@ -29,23 +30,25 @@ export default function Properties() {
     <div className="w-68 bg-white h-screen border-r border-gray-200 p-3 overflow-auto">
       <div className="flex items-center justify-between">
         <div className="text-md font-semibold capitalize"> {selectedElement.type} Properties</div>
-        <div className="flex items-center justify-center rounded-md bg-gray-50 border border-gray-200 cursor-pointer" onClick={() => setSelectedElement(null)}>
+        <div className="flex items-center justify-center gap-2">
+          <button className="flex items-center justify-center p-1 rounded-md bg-red-500 text-white gap-2 hover:bg-red-700 cursor-pointer"
+          onClick={() => {
+            setElements((prev: any[]) => deleteElementById(prev, selectedElement.id));
+            setSelectedElement(null);
+          }}
+        > 
+          <Trash className="w-4 h-4" />
+        </button>
+        <button className="flex items-center justify-center rounded-md bg-gray-50 border border-gray-200 cursor-pointer" onClick={() => setSelectedElement(null)}>
           <X className="w-5 h-5" />
+        </button>
         </div>
       </div>
       <hr className="text-gray-200 mt-2" />
       <div className="flex flex-1 flex-wrap overflow-auto mt-5">
         <PropertyRenderer element={selectedElement} />
       </div>
-      <button className="w-full px-2 py-3 rounded-md flex items-center justify-center font-semibold text-sm mt-2 bg-red-500 text-white gap-2 hover:bg-red-700 cursor-pointer"
-        onClick={() => {
-          setElements((prev: any[]) => deleteElementById(prev, selectedElement.id));
-          setSelectedElement(null);
-        }}
-      > 
-        <Trash className="w-4 h-4" />
-        Remove element
-      </button>
+      
     </div>
   )
 }
