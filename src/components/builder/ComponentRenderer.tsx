728fde4d8b6b7e52c241d1aab8a9fa53c588d52a
Yatik1 <yatiksrivastava1@gmail.com>
2025-05-30 13:34:50 +0530

    refactor: remove hover effects from ComponentRenderer for cleaner UI

--- src/components/builder/ComponentRenderer.tsx
+++ src/components/builder/ComponentRenderer.tsx
@@ -18,21 +18,21 @@ function ComponentRenderer({ element }: ComponentRendererProps) {
     case 'heading':
       const HeadingTag = props.level as keyof JSX.IntrinsicElements;
       if(props.text) return (
-        <div className={`w-fit h-fit p-2 hover:border hover:border-orange-400`} style={{background:props.bgColor}}>
+        <div className={`w-fit h-fit p-2`} style={{background:props.bgColor}}>
           <HeadingTag className={`font-bold`} style={{color:props.color}}>{props.text}</HeadingTag>
         </div>
       )
-      else return <HeadingTag className={`text-gray-500 hover:border hover:border-orange-400`}>Add Heading</HeadingTag>;
+      else return <HeadingTag className={`text-gray-500`}>Add Heading</HeadingTag>;
 
     case 'text':
       if(props.content) {
         return(
-          <div className='w-fit h-fit p-2 hover:border hover:border-blue-400' style={{background:props.bgColor}}>
+          <div className='w-fit h-fit p-2' style={{background:props.bgColor}}>
             <p style={{color:props.color, fontStyle:props.fontStyle, textDecoration:props.decoration}}>{props.content}</p>
           </div>
         )
       }
-      return <p className='text-gray-500 hover:border hover:border-orange-400'>Add content</p>;
+      return <p className='text-gray-500'>Add content</p>;
 
     case 'image':
       return (
@@ -40,11 +40,11 @@ function ComponentRenderer({ element }: ComponentRendererProps) {
           <img
             src={props.src}
             alt="uploaded image"
-            className="rounded-lg object-fit aspect-auto hover:border hover:border-pink-600"
+            className="rounded-lg object-fit aspect-auto"
             style={{width:props.width+"px", height:props.height+"px"}}
           />
         ) : (
-          <button className='w-fit p-2 rounded-md bg-gray-200 text-gray-400 hover:border hover:border-pink-600'>
+          <button className='w-fit p-2 rounded-md bg-gray-200 text-gray-400'>
             <p>Add Image</p>
           </button>
         )
@@ -52,7 +52,7 @@ function ComponentRenderer({ element }: ComponentRendererProps) {
 
     case 'button':
       return (
-         <button className="px-4 py-2 hover:border hover:border-green-400" style={{background:props.bgColor?props.bgColor:"white", color:props.color?props.color : "black", borderRadius:props.rounded+"px"}}> 
+         <button className="px-4 py-2" style={{background:props.bgColor?props.bgColor:"white", color:props.color?props.color : "black", borderRadius:props.rounded+"px"}}> 
           {props.text ? props.text : "Button"}
         </button>
       );
