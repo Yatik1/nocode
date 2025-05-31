56b267678f10570d518f7dea0e711fd2a9165800
Yatik1 <yatiksrivastava1@gmail.com>
2025-05-31 18:07:24 +0530

    refactor: simplify rendering logic in ComponentRenderer for improved readability and maintainability

--- src/components/builder/ComponentRenderer.tsx
+++ src/components/builder/ComponentRenderer.tsx
@@ -1,9 +1,13 @@
 
-import { JSX } from 'react';
 import FlexRow from './layouts/FlexRow';
 import FlexCol from './layouts/FlexCol';
 import { ElementType } from './Canvas';
 import Section from './layouts/Section';
+import Heading from './ui-components/Heading';
+import Text from './ui-components/Text';
+import Image from './ui-components/Image';
+import Button from './ui-components/Button';
+import Divider from './ui-components/Divider';
 
 interface ComponentRendererProps {
   element: ElementType;
@@ -16,91 +20,31 @@ function ComponentRenderer({ element }: ComponentRendererProps) {
 
   switch (type) {
     case 'heading':
-      const HeadingTag = props.level as keyof JSX.IntrinsicElements;
-      if(props.text) return (
-        <div className={`w-fit h-fit p-2`} style={{background:props.bgColor}}>
-          <HeadingTag className={`font-bold`} style={{color:props.color}}>{props.text}</HeadingTag>
-        </div>
-      )
-      else return <HeadingTag className={`text-gray-500`}>Add Heading</HeadingTag>;
-
+      return <Heading {...props} />
+      
     case 'text':
-      if(props.content) {
-        return(
-          <div className='w-fit h-fit p-2' style={{background:props.bgColor}}>
-            <p style={{color:props.color, fontStyle:props.fontStyle, textDecoration:props.decoration}}>{props.content}</p>
-          </div>
-        )
-      }
-      return <p className='text-gray-500'>Add content</p>;
+      return <Text {...props} />
 
     case 'image':
-      return (
-        props.src ? (
-          <img
-            src={props.src}
-            alt="uploaded image"
-            className="rounded-lg object-fit aspect-auto"
-            style={{width:props.width+"px", height:props.height+"px"}}
-          />
-        ) : (
-          <button className='w-fit p-2 rounded-md bg-gray-200 text-gray-400'>
-            <p>Add Image</p>
-          </button>
-        )
-      )
+      return <Image {...props} />
 
     case 'button':
-      return (
-         <button className="px-4 py-2" style={{background:props.bgColor?props.bgColor:"white", color:props.color?props.color : "black", borderRadius:props.rounded+"px"}}> 
-          {props.text ? props.text : "Button"}
-        </button>
-      );
+      return <Button {...props} />
+    
+    case 'divider':
+      return <Divider />
 
     case 'section':
       return (
         <Section props={props} id={id} />
       );
 
-    // case 'navbar':
-    //   return (
-    //     <nav className="flex gap-4 p-4 bg-gray-100 rounded-lg">
-    //       {props.links.map((link: { text: string; href: string }, index: number) => (
-    //         <a
-    //           key={index}
-    //           href={link.href}
-    //           className="text-blue-500 hover:text-blue-600"
-    //         >
-    //           {link.text}
-    //         </a>
-    //       ))}
-    //     </nav>
-    //   );
-
-    case 'divider':
-      return(
-        <div className='w-full p-3'>
-          <hr className='text-gray-200 w-full' />
-        </div>
-      );
-
-      case 'flex-row':
-        return (
-          <FlexRow 
-            props={props}  
-            id={id}
-          />
-        );
+    case 'flex-row':
+        return  <FlexRow props={props}  id={id} />
       
-    
     case 'flex-col':
-      return (
-        <FlexCol 
-          props={props} 
-          id={id}
-        />
-      )
-
+      return <FlexCol props={props} id={id} />
+      
     default:
       return null;
   }
