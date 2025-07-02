3d6894a4dc06db611f2788e2a58bdc9be1756478
Yatik1 <yatiksrivastava1@gmail.com>
2025-07-03 00:24:04 +0530

    feat: update ComponentRenderer to support Canvas2 and clean up unused imports

--- src/components/builder/ComponentRenderer.tsx
+++ src/components/builder/ComponentRenderer.tsx
@@ -1,21 +1,24 @@
-
-import FlexRow from './layouts/FlexRow';
-import FlexCol from './layouts/FlexCol';
-import { ElementType } from './Canvas';
-import Section from './layouts/Section';
+// import FlexRow from './layouts/FlexRow';
+// import FlexCol from './layouts/FlexCol';
+// import Canvas from './Canvas';
+// import Section from './layouts/Section';
 import Heading from './ui-components/Heading';
 import Text from './ui-components/Text';
 import Image from './ui-components/Image';
 import Button from './ui-components/Button';
-import Divider from './ui-components/Divider';
+import Divider from './ui-components/Divider'; 
 
-interface ComponentRendererProps {
-  element: ElementType;
+import Canvas2 from './Canvas2';
+import { CanvasType, ElementType } from '../../types/types';
+
+type ComponentRendererType = {
+  element : ElementType | CanvasType 
 }
 
-function ComponentRenderer({ element }: ComponentRendererProps) {
+function ComponentRenderer({ element }: ComponentRendererType) {
 
   const { id, props, type } = element;
+  const childrens = (element as CanvasType).childrens
 
 
   switch (type) {
@@ -34,17 +37,20 @@ function ComponentRenderer({ element }: ComponentRendererProps) {
     case 'divider':
       return <Divider />
 
-    case 'section':
-      return (
-        <Section props={props} id={id} />
-      );
+    // case 'section':
+    //   return (
+    //     <Section props={props} id={id} />
+    //   );
 
-    case 'flex-row':
-        return  <FlexRow props={props}  id={id} />
+    // case 'flex-row':
+    //     return  <FlexRow props={props}  id={id} />
       
-    case 'flex-col':
-      return <FlexCol props={props} id={id} />
-      
+    // case 'flex-col':
+    //   return <FlexCol props={props} id={id} />
+    
+    case 'canvas':
+      return <Canvas2 props={props} id={id} childrens={childrens} />
+
     default:
       return null;
   }
