ec004734742fc2337f0f63e7069c6507f205b556
Yatik1 <yatiksrivastava1@gmail.com>
2025-07-03 00:25:13 +0530

    feat: update Preview component to handle canvas resizing and use sections for rendering

--- src/components/page/Preview.tsx
+++ src/components/page/Preview.tsx
@@ -1,39 +1,64 @@
-import { useNavigate } from 'react-router-dom'
-import useBuilder from '../../hooks/useBuilder'
-import { X } from 'lucide-react'
-import { ElementType } from '../builder/Canvas'
-import ComponentRenderer from '../builder/ComponentRenderer'
+import { useNavigate } from 'react-router-dom';
+import useBuilder from '../../hooks/useBuilder';
+import { X } from 'lucide-react';
+
+import ComponentRenderer from '../builder/ComponentRenderer';
+import { BuilderContextProps } from '../../context/BuilderContext';
+import { CanvasType } from '../../types/types';
+import { useEffect, useState } from 'react';
 
 function Preview() {
-    
-    const {elements} = useBuilder() as any
-    const navigate = useNavigate()
-    console.log(elements)
+  const { sections } = useBuilder() as BuilderContextProps;
+  const navigate = useNavigate();
+
+  const [canvasSize, setCanvasSize] = useState({
+    width:window.innerWidth, 
+    height:window.innerHeight
+  })
+
+  useEffect(() => {
+    const handleResize = () => {
+      setCanvasSize({
+        width: window.innerWidth,
+        height: window.innerHeight
+      });
+    };
+
+    window.addEventListener('resize', handleResize);
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+  
 
 
   return (
-
-    <div className='w-full h-screen m-0 p-0'>
-    <header className='bg-white items-center justify-between flex w-full p-[0.4rem] border border-b border-gray-200'>
-      <span className='w-9 h-9 flex items-center justify-center bg-white rounded-full'>
-        <img src='logo.png' alt='logo' />
-      </span>
-      <p className='text-[0.856rem]'>Preview</p>
-      <button className="flex items-center justify-center border border-gray-400 rounded-lg text-sm p-1" onClick={() => navigate("/")}>
-        <X className='w-4 h-4' />
-      </button>
-    </header>
-    <div className='bg-white flex flex-col w-full h-screen'>
-    {elements.map((element: ElementType) => (
-    <div
-      key={element.id}
-    >
-      <ComponentRenderer element={element} />
+    <div>
+      <header className='bg-white items-center justify-between flex w-full p-[0.4rem] border border-b border-gray-200'>
+        <span className='w-9 h-9 flex items-center justify-center bg-white rounded-full'>
+          <img src='logo.png' alt='logo' />
+        </span>
+        <p className='text-[0.856rem]'>Preview</p>
+        <button
+          className='flex items-center justify-center border border-gray-400 rounded-lg text-sm p-1'
+          onClick={() => navigate('/')}
+        >
+          <X className='w-4 h-4' />
+        </button>
+      </header>
+      <div className='bg-white relative flex flex-col items-center -gap-0.5'
+      style={{ width: canvasSize.width, height: canvasSize.height }}
+      >
+        {sections.map((element: CanvasType) => (
+          <div
+            key={element.id}
+          >
+            <ComponentRenderer element={element} />
+          </div>
+        ))}
+      </div>
     </div>
-  ))}
-    </div>
-    </div>
-  )
+  );
 }
 
-export default Preview
+export default Preview;
