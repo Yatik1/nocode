cc1b8dc1dc7a65f669b31e3920a68fcf95d86198
Yatik1 <yatiksrivastava1@gmail.com>
2025-07-23 19:28:44 +0530

    refactor: update Button and Image components to use optional width and height units for improved flexibility

--- src/components/builder/ui-components/Image.tsx
+++ src/components/builder/ui-components/Image.tsx
@@ -4,17 +4,23 @@ function Image(props : {
     src?:string,
     width?:string, 
     height?:string,
-    widthUnit:string, 
-    heightUnit:string,
+    widthUnit?:string, 
+    heightUnit?:string,
     rounded?:string
 }) {
+
+  const {
+    widthUnit = "px",
+    heightUnit = "px"
+  } = props;
+
   return (
     props.src ? (
           <img
             src={props.src}
             alt="uploaded image"
             className="object-fit aspect-auto"
-            style={{width:props.width+props.widthUnit, height:props.height+props.heightUnit, borderRadius:props.rounded+"px"}}
+            style={{width:props.width+widthUnit, height:props.height+heightUnit, borderRadius:props.rounded+"px"}}
           />
         ) : (
           <button className='w-fit p-2 rounded-md bg-gray-200 text-gray-400'>
