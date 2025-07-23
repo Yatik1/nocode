36a9566a942604c3c904b9c10823c1be934f42c8
Yatik1 <yatiksrivastava1@gmail.com>
2025-07-23 19:07:10 +0530

    refactor: update TextChangeUtil to use textarea for text input, enhancing user experience and improving text handling

--- src/components/builder/properties-form/utils/TextChangeUtil.tsx
+++ src/components/builder/properties-form/utils/TextChangeUtil.tsx
@@ -8,15 +8,14 @@ function TextChangeUtil({element}:{element:CanvasType | ElementType}) {
     const {props} = element
     const {updateElementProps} = useBuilder() as BuilderContextProps
 
-    function onTextChange(e:React.ChangeEvent<HTMLInputElement>) {
+    function onTextChange(e:React.ChangeEvent<HTMLTextAreaElement>) {
         updateElementProps({...element, props:{...props, text:e.target.value}})
     }
 
   return (
     <>
         <label className="text-sm font-semibold">Text</label>
-        <input 
-            type="text"
+        <textarea 
             value={props.text}
             onChange={onTextChange}
             placeholder="Text"
