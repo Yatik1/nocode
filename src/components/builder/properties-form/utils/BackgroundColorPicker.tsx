2b12855194ec04c9e9f880c6904babeff8691be6
Yatik1 <yatiksrivastava1@gmail.com>
2025-09-15 12:53:34 +0530

    refactor: clean up BackgroundColorPicker component and improve readability

--- src/components/builder/properties-form/utils/BackgroundColorPicker.tsx
+++ src/components/builder/properties-form/utils/BackgroundColorPicker.tsx
@@ -1,50 +1,56 @@
-import React from 'react'
-import ColorPicker from 'react-best-gradient-color-picker'
+import ColorPicker from 'react-best-gradient-color-picker';
 import { CanvasType, ElementType } from '../../../../types/types';
 import useBuilder from '../../../../hooks/useBuilder';
 import { BuilderContextProps } from '../../../../context/BuilderContext';
+import { Ban, Eye, EyeClosed } from 'lucide-react';
+import { rgbaToHex } from './utils';
 
-function BackgroundColorPicker({element}:{element : ElementType | CanvasType}) {
+function BackgroundColorPicker({ element }: { element: ElementType | CanvasType }) {
+  const { props } = element;
+  const { updateElementProps, isBgColorPicker, setIsBgColorPicker, setIsColorPicker } = useBuilder() as BuilderContextProps;
 
-
-    const {props} = element
-    const {updateElementProps, isBgColorPicker, setIsBgColorPicker,setIsColorPicker} = useBuilder() as BuilderContextProps;
-
-
-    function onBgChange(color:string) {
-        updateElementProps({
-          ...element,
-          props:{...props, background: color || "none"}
-        })
-      }
-
-      function onBgColorChangeFromInput(e:React.ChangeEvent<HTMLInputElement>) {
-      updateElementProps({
-        ...element,
-        props:{...props, background: e.target.value}
-      })
+  function handleClick() {
+    if(props.background !== 'none') {
+      updateElementProps({...element, props:{...props, background:'none'}});
+    } else {
+      updateElementProps({...element, props:{...props, background:'#FFFFFF'}});
     }
+  }
 
+  function onBgChange(color: string) {
+    const hexColor = color.startsWith('linear') ? color : rgbaToHex(color);
+    updateElementProps({
+      ...element,
+      props: { ...props, background: hexColor },
+    });
+  }
+  
   return (
-    <>
-        <label className="text-[#272727] text-sm relative">Background</label>
-      <div className="flex flex-col gap-2 justify-center items-start relative">
-        <div className="flex items-center justify-between gap-2">
-        <div className={`w-7 h-7 rounded-full border border-stone-400 cursor-pointer`} style={{background:props.background}} onClick={() => {setIsColorPicker(false);setIsBgColorPicker(prev=>!prev)}} />
-         <input
-          type="text"
-          value={props.background}
-          placeholder="#000000"
-          className="border border-gray-300 rounded-md py-2 px-3 text-sm"
-          onChange={onBgColorChangeFromInput}
-        />
+    <div className='flex flex-col gap-1'>
+      <label className="text-[#272727] text-sm font-medium relative">Background</label>
+      <div className="flex items-center justify-between gap-1 w-[15rem]">
+        <div
+          className="flex items-center justify-between gap-1.5 py-1 px-2 bg-[#F4F4F4] rounded-[4px]"
+          onClick={() => {
+            setIsColorPicker(false);
+            setIsBgColorPicker((prev) => !prev);
+          }}
+        >
+          {props.background === 'none' ? <Ban strokeWidth={1.5} size={15} /> : <div className={`w-[15px] h-[15px] rounded-[2px]`} style={{ background: props.background }} />}
+          <p className="text-[#707070] text-sm w-25 truncate capitalize">
+            {props.background?.startsWith('linear') ? 'Linear' : props.background}
+          </p>
+        </div>
 
+        <div className="flex items-center justify-center hover:bg-[#f4f4f4] rounded-sm pointer-cursor p-1" onClick={handleClick}>
+          {props.background === 'none' ? <EyeClosed strokeWidth={1.5} size={17} /> : <Eye strokeWidth={1.5} size={17} />}
         </div>
       </div>
-      
+
       {isBgColorPicker && (
         <ColorPicker
           value={props.background}
+          showHexAlpha={false}
           onChange={onBgChange}
           disableDarkMode={true}
           width={310}
@@ -52,10 +58,8 @@ function BackgroundColorPicker({element}:{element : ElementType | CanvasType}) {
           className="absolute right-[19vw] border border-gray-300 rounded-lg flex items-center justify-center overflow-hidden px-[10rem] py-2"
         />
       )}
-      
-      
-    </>
-  )
+    </div>
+  );
 }
 
-export default BackgroundColorPicker
+export default BackgroundColorPicker;
