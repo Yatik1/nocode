643c7c501906c568761ea5aaa271966b783c6e62
Yatik1 <yatiksrivastava1@gmail.com>
2025-09-15 12:53:40 +0530

    refactor: simplify TextColorPicker component and enhance color handling

--- src/components/builder/properties-form/utils/TextColorPicker.tsx
+++ src/components/builder/properties-form/utils/TextColorPicker.tsx
@@ -2,42 +2,44 @@ import ColorPicker from 'react-best-gradient-color-picker'
 import { ElementType } from '../../../../types/types';
 import useBuilder from '../../../../hooks/useBuilder';
 import { BuilderContextProps } from '../../../../context/BuilderContext';
+import { Minus } from 'lucide-react';
+import { rgbaToHex } from './utils';
 
 function TextColorPicker({element}:{element : ElementType}) {
 
     const {props} = element
     const {updateElementProps, isColorPicker, setIsColorPicker, setIsBgColorPicker} = useBuilder() as BuilderContextProps;
 
+    function handleClick() {
+      updateElementProps({...element, props:{...props, color:'#000000'}});
+    }
 
     function onColorChange(color:string) {
         updateElementProps({
           ...element,
-          props:{...props, color: color}
+          props:{...props, color: rgbaToHex(color)}
         })
     }
 
-    function onColorChangeFromInput(e:React.ChangeEvent<HTMLInputElement>) {
-      updateElementProps({
-        ...element,
-        props:{...props, color: e.target.value}
-      })
-    }
-    
-
   return (
-    <>
-        <label className="text-sm font-semibold relative">Text color</label>
-      <div className="flex flex-col gap-2 justify-center items-start relative">
-        <div className="flex items-center justify-between gap-2">
-        <div className={`w-7 h-7 rounded-full border border-stone-400 cursor-pointer`} style={{background:props.color}} onClick={() => {setIsBgColorPicker(false);setIsColorPicker(prev=>!prev)}} />
-        <input
-          type="text"
-          value={props.color}
-          placeholder="#000000"
-          className="border border-gray-300 rounded-md py-2 px-3 text-sm"
-          onChange={onColorChangeFromInput}
-        />
+    <div className='flex flex-col gap-1'>
+      <label className="text-[#272727] text-sm font-medium relative">Color</label>
+      <div className="flex items-center justify-between gap-1 w-[15rem]">
+        <div
+          className="flex items-center justify-between gap-1.5 py-1 px-2 bg-[#F4F4F4] rounded-[4px]"
+          onClick={() => {
+            setIsBgColorPicker(false);
+            setIsColorPicker((prev) => !prev);
+          }}
+        >
+          <div className={`w-[15px] h-[15px] rounded-[2px]`} style={{ background: props.color }} />
+          <p className="text-[#707070] text-sm w-25 truncate capitalize">
+            {props.color}
+          </p>
+        </div>
 
+        <div className="flex items-center justify-center hover:bg-[#f4f4f4] rounded-sm pointer-cursor p-1" onClick={handleClick}>
+          <Minus strokeWidth={1.5} size={17} />
         </div>
       </div>
       
@@ -52,7 +54,7 @@ function TextColorPicker({element}:{element : ElementType}) {
                 className="absolute right-[19vw] border border-gray-300 rounded-lg flex items-center justify-center overflow-hidden px-[10rem] py-2"
               />
             )}
-    </>
+    </div>
   )
 }
 
