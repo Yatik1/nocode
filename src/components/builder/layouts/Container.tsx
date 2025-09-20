4a0ae958447647ff8ab5f5730faea62d58e0d427
Yatik1 <yatiksrivastava1@gmail.com>
2025-09-20 17:42:44 +0530

    ::rfefactor Container by improving dragging experience

--- src/components/builder/layouts/Container.tsx
+++ src/components/builder/layouts/Container.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import { CanvasType, ElementType } from '../../../types/types';
 import useBuilder from '../../../hooks/useBuilder';
 import { BuilderContextProps } from '../../../context/BuilderContext';
@@ -8,14 +8,16 @@ import ComponentRenderer from '../ComponentRenderer';
 import { handleElementDragStart } from '../../../utils/handleElementDragStart';
 
 function Container({ id, props, childrens }: CanvasType) {
+  const { updatePageContent, selectedElement, setSelectedElement, setElements, transform } =
+    useBuilder() as BuilderContextProps;
 
-  const { updatePageContent, selectedElement, setSelectedElement, setElements } = useBuilder() as BuilderContextProps;
+  const containerRef = useRef<HTMLElement>(null); 
 
   const handleDrop = (e: React.DragEvent) => {
     e.preventDefault();
     e.stopPropagation();
 
-    const childElementType = e.dataTransfer.getData("componentId");
+    const childElementType = e.dataTransfer.getData('componentId');
     if (!childElementType) return;
 
     const newElement: ElementType = {
@@ -36,48 +38,63 @@ function Container({ id, props, childrens }: CanvasType) {
   return (
     <section
       id={id}
-      className="relative"
+      className="relative overflow-hidden"
       data-section-id={id}
       onDrop={handleDrop}
       onClick={(e) => {
         e.preventDefault();
         e.stopPropagation();
-        setSelectedElement({ id, props, childrens: childrens || [], type: "container" });
+        setSelectedElement({ id, props, childrens: childrens || [], type: 'container' });
       }}
       onDragOver={handleDragOver}
       style={{
         background: props.background,
         width: props.width + props.widthUnit,
         height: props.height + props.heightUnit,
-        borderRadius: props.rounded + "px",
+        borderRadius: props.rounded + 'px',
         opacity: props.opacity,
       }}
+      ref={containerRef} // Assign the ref to the container
     >
-      {childrens && childrens.map((element: ElementType) => (
-        <div
-          key={element.id}
-          data-element-id={element.id}
-          onClick={(e: React.MouseEvent) => {
-            e.preventDefault();
-            e.stopPropagation();
-            setSelectedElement(element);
-          }}
-          onMouseDownCapture={(e) => {
-            e.preventDefault();
-            e.stopPropagation();
-            handleElementDragStart(e, element.id, childrens, setElements);
-          }}
-          className={`${selectedElement?.id === element.id ? "border border-blue-500" : ""} absolute w-fit`}
-          style={{
-            top: element.y,
-            left: element.x,
-            cursor: "move",
-            userSelect: "none",
-          }}
-        >
-          <ComponentRenderer element={element} />
-        </div>
-      ))}
+      {childrens &&
+        childrens.map((element: ElementType) => (
+          <div
+            key={element.id}
+            data-element-id={element.id}
+            onClick={(e: React.MouseEvent) => {
+              e.preventDefault();
+              e.stopPropagation();
+              setSelectedElement(element);
+            }}
+            onMouseDownCapture={(e) => {
+              e.preventDefault();
+              e.stopPropagation();
+              if (containerRef.current) {
+                handleElementDragStart(
+                  e,
+                  element.id,
+                  childrens,
+                  setElements,
+                  {
+                    x: transform.x ,
+                    y: transform.y,
+                    scale: transform.scale,
+                  },
+                  containerRef.current
+                );
+              }
+            }}
+            className={`${selectedElement?.id === element.id && 'border border-blue-500'} absolute w-fit`}
+            style={{
+              top: element.y,
+              left: element.x,
+              cursor: 'move',
+              userSelect: 'none',
+            }}
+          >
+            <ComponentRenderer element={element} />
+          </div>
+        ))}
     </section>
   );
 }
