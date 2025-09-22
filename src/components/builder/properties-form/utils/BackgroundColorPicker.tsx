33a0f9e3fff9e1ee6ee9b1b31b61796f819676ac
Yatik1 <yatiksrivastava1@gmail.com>
2025-09-22 14:26:23 +0530

    updated Color Picker to the function Component

--- src/components/builder/properties-form/utils/BackgroundColorPicker.tsx
+++ src/components/builder/properties-form/utils/BackgroundColorPicker.tsx
@@ -1,21 +1,12 @@
-import ColorPicker from 'react-best-gradient-color-picker';
 import { CanvasType, ElementType } from '../../../../types/types';
 import useBuilder from '../../../../hooks/useBuilder';
 import { BuilderContextProps } from '../../../../context/BuilderContext';
-import { Ban, Eye, EyeClosed } from 'lucide-react';
-import { rgbaToHex } from './utils';
+import ColorPickerUtil from './ColorPickerUtil';
 
 function BackgroundColorPicker({ element }: { element: ElementType | CanvasType }) {
   const { props } = element;
-  const { updateElementProps, isBgColorPicker, setIsBgColorPicker, setIsColorPicker } = useBuilder() as BuilderContextProps;
+  const { updateElementProps, activeColorPicker, setActiveColorPicker } = useBuilder() as BuilderContextProps;
 
-  function handleClick() {
-    if(props.background !== 'none') {
-      updateElementProps({...element, props:{...props, background:'none'}});
-    } else {
-      updateElementProps({...element, props:{...props, background:'#FFFFFF'}});
-    }
-  }
 
   function onBgChange(color: string) {
     updateElementProps({
@@ -27,36 +18,12 @@ function BackgroundColorPicker({ element }: { element: ElementType | CanvasType 
   return (
     <div className='flex flex-col gap-1'>
       <label className="text-[#272727] text-sm font-medium relative">Background</label>
-      <div className="flex items-center justify-between gap-1 w-[15rem]">
-        <div
-          className="flex items-center justify-between gap-1.5 py-1 px-2 bg-[#F4F4F4] rounded-[4px]"
-          onClick={() => {
-            setIsColorPicker(false);
-            setIsBgColorPicker((prev) => !prev);
-          }}
-        >
-          {props.background === 'none' ? <Ban strokeWidth={1.5} size={15} /> : <div className={`w-[15px] h-[15px] rounded-[2px]`} style={{ background: props.background }} />}
-          <p className="text-[#707070] text-sm w-25 truncate capitalize">
-            {props.background?.startsWith('linear') ? 'Linear' : rgbaToHex(props.background)}
-          </p>
-        </div>
-
-        <div className="flex items-center justify-center hover:bg-[#f4f4f4] rounded-sm pointer-cursor p-1" onClick={handleClick}>
-          {props.background === 'none' ? <EyeClosed strokeWidth={1.5} size={17} /> : <Eye strokeWidth={1.5} size={17} />}
-        </div>
-      </div>
-
-      {isBgColorPicker && (
-        <ColorPicker
-          value={props.background}
-          showHexAlpha={false}
-          onChange={onBgChange}
-          disableDarkMode={true}
-          width={310}
-          height={180}
-          className="absolute z-50 left-[-120%] border border-gray-300 rounded-lg flex items-center justify-center overflow-hidden px-[10rem] py-2"
-        />
-      )}
+      <ColorPickerUtil 
+        value={props.background}
+        open={activeColorPicker === 'background'}
+        onToggleVisibility={(open) => setActiveColorPicker(open ? "background" : null)}
+        onChange={onBgChange}
+      />
     </div>
   );
 }
--- src/components/builder/properties-form/utils/TextColorPicker.tsx
+++ src/components/builder/properties-form/utils/TextColorPicker.tsx
@@ -1,18 +1,17 @@
-import ColorPicker from 'react-best-gradient-color-picker'
 import { ElementType } from '../../../../types/types';
 import useBuilder from '../../../../hooks/useBuilder';
 import { BuilderContextProps } from '../../../../context/BuilderContext';
-import { Minus } from 'lucide-react';
 import { rgbaToHex } from './utils';
+import ColorPickerUtil from './ColorPickerUtil';
 
 function TextColorPicker({element}:{element : ElementType}) {
 
     const {props} = element
-    const {updateElementProps, isColorPicker, setIsColorPicker, setIsBgColorPicker} = useBuilder() as BuilderContextProps;
+    const {updateElementProps, activeColorPicker, setActiveColorPicker} = useBuilder() as BuilderContextProps;
 
-    function handleClick() {
-      updateElementProps({...element, props:{...props, color:'#000000'}});
-    }
+    // function handleClick() {
+    //   updateElementProps({...element, props:{...props, color:'#000000'}});
+    // }
 
     function onColorChange(color:string) {
         updateElementProps({
@@ -24,36 +23,13 @@ function TextColorPicker({element}:{element : ElementType}) {
   return (
     <div className='flex flex-col gap-1'>
       <label className="text-[#272727] text-sm font-medium relative">Color</label>
-      <div className="flex items-center justify-between gap-1 w-[15rem]">
-        <div
-          className="flex items-center justify-between gap-1.5 py-1 px-2 bg-[#F4F4F4] rounded-[4px]"
-          onClick={() => {
-            setIsBgColorPicker(false);
-            setIsColorPicker((prev) => !prev);
-          }}
-        >
-          <div className={`w-[15px] h-[15px] rounded-[2px]`} style={{ background: props.color }} />
-          <p className="text-[#707070] text-sm w-25 truncate capitalize">
-            {props.color}
-          </p>
-        </div>
-
-        <div className="flex items-center justify-center hover:bg-[#f4f4f4] rounded-sm pointer-cursor p-1" onClick={handleClick}>
-          <Minus strokeWidth={1.5} size={17} />
-        </div>
-      </div>
-      
-      {isColorPicker && (
-              <ColorPicker
-                value={props.color}
-                onChange={onColorChange}
-                disableDarkMode={true}
-                hideControls={true}
-                width={310}
-                height={180}
-                className="absolute z-50 left-[-120%] border border-gray-300 rounded-lg flex items-center justify-center overflow-hidden px-[10rem] py-2"
-              />
-            )}
+      <ColorPickerUtil 
+        value={props.color || "#000000"}
+        open={activeColorPicker === 'text'}
+        onToggleVisibility={(open) => setActiveColorPicker(open ? "text" : null)}
+        onChange={onColorChange}
+        type="text"
+      />
     </div>
   )
 }
